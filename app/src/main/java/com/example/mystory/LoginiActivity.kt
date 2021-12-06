9e1c62a84b3d1b1cb50309eebaec5d8e63972ee5
Basma-AlKhameri <basma21970@gmail.com>
2021-12-06 15:18:26 +0300

    splash Activity

---
+++ app/src/main/java/com/example/mystory/LoginiActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mystory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginiActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_logini)
+    }
+}
