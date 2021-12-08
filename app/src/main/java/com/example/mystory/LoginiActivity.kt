4074268dfe2f98541145b1a7d4fd7831d5ffe7d9
Basma-AlKhameri <basma21970@gmail.com>
2021-12-08 12:38:25 +0300

    login activity

--- app/src/main/java/com/example/mystory/LoginiActivity.kt
+++ app/src/main/java/com/example/mystory/LoginiActivity.kt
@@ -2,10 +2,43 @@ package com.example.mystory
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.mystory.databinding.ActivityLoginiBinding
 
 class LoginiActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginiBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_logini)
+
+        binding = ActivityLoginiBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        login()
+    }
+
+    private fun login() {
+        var UserArray:ArrayList<User> = ArrayList()
+        UserArray.add(User("basma@gmail.com", "123"))
+        UserArray.add(User("b@gmail.com", "1234"))
+        UserArray.add(User("s@gmail.com", "12345"))
+
+        binding.loginButton.setOnClickListener {
+
+          val username=  binding.UserName.text.toString()
+            val pass = binding.pass.text.toString()
+            val user= User(username, pass)
+
+            for(uA in UserArray){
+                if(uA.Email == user.Email && uA.pass== user.pass){
+                    Toast.makeText(this,"welcome", Toast.LENGTH_SHORT).show()
+                    break
+                }else{
+                    Toast.makeText(this,"check your data", Toast.LENGTH_SHORT).show()
+                }
+        }
+
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/mystory/User.kt
@@ -0,0 +1,3 @@
+package com.example.mystory
+
+data class User(val Email:String, val pass:String)
